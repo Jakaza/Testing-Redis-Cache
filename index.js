ff70331f2517b7f18ba2f6f7d1c8ae6a444de326
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-02-25 15:06:28 +0000

    install and connect ioredis.

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
-const { createClient } = require('redis');
+const Redis = require("ioredis");
 const axios = require('axios');
 const redisPass = process.env['REDIS_PASS']
 const weatherApi = process.env['WEATHER_API']
 
-const client = createClient({
-    password: redisPass,
-    socket: {
-        host: 'redis-15970.c241.us-east-1-4.ec2.cloud.redislabs.com',
-        port: 15970
-    }
+const redis = new Redis({
+  port: 15970, // Redis port
+  host: "redis-15970.c241.us-east-1-4.ec2.cloud.redislabs.com", // Redis host
+  username: "default", // needs Redis >= 6
+  password: redisPass,
+  db: 0, // Defaults to 0
 });
+
+// redis.set("mykey", "value");
+
+redis.get("myk", (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result); // Prints "value"
+  }
+});
+
 
 const endPoint = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi}`;
 
 
-const getCurrentWeather = async (city) =>{
+async function getCurrentWeather (city){
+  try{
 
-  let data = await axios.get(endPoint(city))
-  data = await data.data
-
-  data.source = "API";
-
-  console.log(data)
-  
+    // check if we have a cheched.
+    let weather = await redis.get(`weather:${city}`);
+    if(weather){
+      
+    }else{
+      console.log()
+    }
+    
+    let data = await axios.get(endPoint(city))
+    data = await data.data
+    data.source = "API";
+    return data
+  }catch(error){
+    return new Error(error)
+  }
 }
 
-const cityName = "London";
-getCurrentWeather(cityName)
+(async() => {
+
+// const t0 = new Date().getTime();
+// const cityName = "London";
+// let weather = await getCurrentWeather(cityName)
+// const t1 = new Date().getTime();
+// weather.responseTime = `${t1-t0}ms`;
+// console.log(weather);
+
+})();
+
+
+
+
 
