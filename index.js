b7768a2019a1260dbf2c50242026e706acdb5706
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-02-25 15:27:04 +0000

    Some details about Redis cache

--- index.js
+++ index.js
@@ -11,48 +11,45 @@ const redis = new Redis({
   db: 0, // Defaults to 0
 });
 
-// redis.set("mykey", "value");
-
-redis.get("myk", (err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(result); // Prints "value"
-  }
-});
+// redis.set("key", "value")
+// set cache wtih expiry date
+// redis.set("key","value", "EX", 60 * 60 * 24) expiry 1 Day
+// redis.get("key") return promise
 
 
 const endPoint = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi}`;
 
 
-async function getCurrentWeather (city){
-  try{
+async function getCurrentWeather(city) {
+  try {
 
-    // check if we have a cheched.
-    let weather = await redis.get(`weather:${city}`);
-    if(weather){
-      
-    }else{
-      console.log()
+    // check if weather for city has been cheched.
+    let weatherCache = await redis.get(`weather:${city}`);
+    if (weatherCache) {
+      weatherCache = JSON.parse(weatherCache);
+      weatherCache.source = "CACHE";
+      return weatherCache;
     }
-    
+
     let data = await axios.get(endPoint(city))
     data = await data.data
+    redis.set(`weather:${city}`, JSON.stringify(data))
     data.source = "API";
     return data
-  }catch(error){
+  } catch (error) {
     return new Error(error)
   }
 }
 
-(async() => {
+(async () => {
 
-// const t0 = new Date().getTime();
-// const cityName = "London";
-// let weather = await getCurrentWeather(cityName)
-// const t1 = new Date().getTime();
-// weather.responseTime = `${t1-t0}ms`;
-// console.log(weather);
+  const t0 = new Date().getTime();
+  const cityName = "London";
+  const cityName2 = "Pretoria";
+  let weather = await getCurrentWeather(cityName2)
+  const t1 = new Date().getTime();
+  weather.responseTime = `${t1 - t0}ms`;
+  console.log(weather);
 
 })();
 
